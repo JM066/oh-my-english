0bb0139438be52394b938f5ecf6fd884440efca9
JM066 <56789447+JM066@users.noreply.github.com>
2023-11-12 22:01:26 +0900

    Added Dropzone

--- src/view/components/atoms/ProgressBar/index.tsx
+++ src/view/components/atoms/ProgressBar/index.tsx
@@ -1,6 +1,19 @@
-import { type ReactNode } from 'react'
+import { type ReactNode, type ComponentProps } from 'react'
 import { useProgressBar, type AriaProgressBarProps } from 'react-aria'
 import { twMerge } from '../../../utils/tailwind'
+
+const Height = {
+  Default: 'tw-h-1',
+  Medium: 'tw-h-2',
+} as const
+
+const Color = {
+  Primary: 'tw-bg-bprimary',
+  Secondary: 'tw-bg-bsecondary',
+  Success: 'tw-bg-green-500',
+  Danger: 'tw-bg-rose-500',
+  Default: 'tw-bg-gray-300',
+} as const
 
 interface Props {
   label?: ReactNode
@@ -9,31 +22,41 @@ interface Props {
   maxValue: number
   options?: AriaProgressBarProps
   showValueLabel?: boolean
+  color?: keyof typeof Color
+  height?: keyof typeof Height
+  labelClassName?: string
 }
 function ProgressBar(props: Props): JSX.Element {
-  const { label, showValueLabel = !!label, options, value, minValue = 0, maxValue = 100 } = props
+  const {
+    label,
+    showValueLabel = !!label,
+    options,
+    value,
+    minValue = 0,
+    maxValue = 100,
+    color = 'Default',
+    height = 'Default',
+    labelClassName,
+  } = props
 
-  const { progressBarProps, labelProps } = useProgressBar({ ...options, value, minValue, maxValue })
+  const { progressBarProps, labelProps } = useProgressBar({
+    ...options,
+    label,
+    value,
+    minValue,
+    maxValue,
+  })
 
   const percentage = (value - minValue) / (maxValue - minValue)
   const barWidth = `${Math.round(percentage * 100)}%`
   return (
-    <div
-      {...progressBarProps}
-      className={twMerge(
-        progressBarProps.className,
-        // Height[height],
-        // Width[width],
-        // 'tw-inline-block tw-align-top',
-        // className,
-      )}
-    >
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+    <div {...progressBarProps}>
+      <div className={twMerge('tw-bg-gray-300 tw-w-full', Height[height])}>
+        <div className={twMerge(Color[color], Height[height])} style={{ width: barWidth }} />
+      </div>
+      <div className={twMerge('tw-flex tw-items-center tw-justify-between', labelClassName)}>
         {label && <span {...labelProps}>{label}</span>}
         {showValueLabel && <span>{progressBarProps['aria-valuetext']}</span>}
-      </div>
-      <div style={{ height: 10, background: 'lightgray' }}>
-        <div style={{ width: barWidth, height: 10, background: 'orange' }} />
       </div>
     </div>
   )
--- src/view/pages/Test/index.tsx
+++ src/view/pages/Test/index.tsx
@@ -4,31 +4,39 @@ import useFetchTests from '../../hooks/api/useFetchTests'
 import LoadingCard from '../../components/atoms/LoadingCard'
 import ProgressBar from '../../components/atoms/ProgressBar'
 import Text from '../../components/atoms/Text'
+import Button from '../../components/atoms/Button'
 
 function Test(): JSX.Element | null {
   const { id } = useParams()
-  const [currentIdx, setCurrentIdx] = useState<number>(0)
+  const [current, setCurrent] = useState<number>(0)
   const { tests, isLoading } = useFetchTests(id)
+  console.log('test', tests)
   if (!tests) return null
   return (
     <div>
-      <ProgressBar
-        label={<Text as='h2' text='hihi' />}
-        value={currentIdx}
-        maxValue={tests?.length}
-        minValue={0}
-        options={{ 'aria-label': `progress bar ${id}` }}
-      />
       {!isLoading ? (
-        tests?.map((test, idx) => {
-          const key = `${test.id}_${idx}`
-          setCurrentIdx(idx)
-          return <div key={key}>{test.answerKr}</div>
-        })
+        <>
+          <ProgressBar
+            label={<Text as='h2' text={`${current + 1}/${tests.length}`} />}
+            showValueLabel={false}
+            value={current + 1}
+            maxValue={tests.length}
+            minValue={0}
+            color='Success'
+            labelClassName='tw-justify-end'
+            options={{ 'aria-label': `progress bar ${id}` }}
+          />
+          <Text text={tests[current].answerKr} />
+          <Button
+            theme='Inverted'
+            onPress={() => setCurrent((prev) => Math.min(prev + 1, tests.length))}
+          >
+            <Text text='Next' />
+          </Button>
+        </>
       ) : (
         <LoadingCard />
       )}
-      <div>ni</div>
     </div>
   )
 }
