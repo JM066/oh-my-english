eb55f7f90e6ab4cea9c9fb62304337d69495d7ea
JM066 <56789447+JM066@users.noreply.github.com>
2024-02-20 19:48:06 +0000

    Text input type fixed temporarily

--- src/view/components/molecules/TextInput/index.tsx
+++ src/view/components/molecules/TextInput/index.tsx
@@ -1,43 +1,41 @@
 import { memo } from 'react'
 
-import { type Control, Controller, type FieldValues, type Path } from 'react-hook-form'
+import { type UseControllerProps, useController } from 'react-hook-form'
 import { Input, Label, TextField, FieldError } from 'react-aria-components'
 import Text from '../../atoms/Text'
+import { type Login } from '../../../types/User'
 
-export interface Props<T> {
-  control: Control<FieldValues, T>
-  label: Path<T>
+export interface Props {
+  name: string
+  label: string
 }
 
-function TextInput<T>({ control, label }: Props<T>) {
+function TextInput<T extends Login>(props: Props & UseControllerProps<T>): JSX.Element {
+  // Todo: Need to fix the generic type
+  const { name, control, label } = props
+  const {
+    field: { ref, value, onChange, onBlur },
+    fieldState: { invalid, error },
+  } = useController<T>({ name, control })
+
   return (
-    <Controller
-      control={control}
-      name={label}
-      rules={{ required: `${label} is required` }}
-      render={({
-        field: { name, value, onChange, onBlur, ref },
-        fieldState: { invalid, error },
-      }) => (
-        <TextField
-          name={name}
-          value={value}
-          onChange={onChange}
-          onBlur={onBlur}
-          isRequired
-          validationBehavior='aria'
-          isInvalid={invalid}
-        >
-          <Label>
-            <Text text={label} color='Gray800' />
-          </Label>
-          <Input ref={ref} />
-          <FieldError>
-            <Text text={error?.message} color='Red' />
-          </FieldError>
-        </TextField>
-      )}
-    />
+    <TextField
+      name={name}
+      value={value}
+      onChange={onChange}
+      onBlur={onBlur}
+      isRequired
+      validationBehavior='aria'
+      isInvalid={invalid}
+    >
+      <Label>
+        <Text text={label} color='Gray800' />
+      </Label>
+      <Input ref={ref} />
+      <FieldError>
+        <Text text={error?.message} color='Red' />
+      </FieldError>
+    </TextField>
   )
 }
 
--- src/view/components/organisms/Form/index.tsx
+++ src/view/components/organisms/Form/index.tsx
@@ -1,7 +1,7 @@
 import { useForm } from 'react-hook-form'
 import { Button } from 'react-aria-components'
 import TextInput from '../../molecules/TextInput'
-import authService from '../../../../services/auth'
+// import authService from '../../../../services/auth'
 import { type Login } from '../../../types/User'
 
 function Form() {
@@ -12,13 +12,13 @@ function Form() {
     },
   })
   const onSubmit = async (data: Login) => {
-    const { email, password } = data
-    const login = await authService.userLogin()
+    // const { email, password } = data
+    // const login = await authService.userLogin()
   }
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <TextInput control={control} label='email' />
-      <TextInput control={control} label='password' />
+      <TextInput name='email' label='email' control={control} />
+      <TextInput name='password' label='password' control={control} />
       <Button type='submit'>Submit</Button>
     </form>
   )
